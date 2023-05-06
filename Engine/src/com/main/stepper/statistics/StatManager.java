224011879d5bed4cafab765f3974f72176b159b1
Omer Erlichman <omer.er10@gmail.com>
2023-05-06 16:16:47 +0300

    Removed failed runs from statistics

--- Engine/src/com/main/stepper/statistics/StatManager.java
+++ Engine/src/com/main/stepper/statistics/StatManager.java
@@ -2,6 +2,8 @@ package com.main.stepper.statistics;
 
 import com.main.stepper.engine.executor.api.IFlowRunResult;
 import com.main.stepper.engine.executor.api.IStepRunResult;
+import com.main.stepper.flow.definition.api.FlowResult;
+import com.main.stepper.step.definition.api.StepResult;
 
 import java.io.Serializable;
 import java.time.Duration;
@@ -49,7 +51,7 @@ public class StatManager implements Serializable {
     }
 
     public Duration getFlowRunAverageTimeMS(String flowName){
-        List<IFlowRunResult> flowRunResults = this.flowRunResults.stream().filter(f->f.name().equals(flowName)).collect(Collectors.toList());
+        List<IFlowRunResult> flowRunResults = this.flowRunResults.stream().filter(f->f.name().equals(flowName) && !f.result().equals(FlowResult.FAILURE)).collect(Collectors.toList());
         if(flowRunResults.isEmpty())
             return Duration.ZERO;
         return Duration.ofMillis((long)flowRunResults.stream().map(f->f.duration()).mapToLong(Duration::toMillis).average().orElse(0));
@@ -57,18 +59,18 @@ public class StatManager implements Serializable {
     }
 
     public Duration getStepRunAverageTimeMS(String stepName){
-        List<IStepRunResult> stepRunResults = this.stepRunResults.stream().filter(f->f.name().equals(stepName)).collect(Collectors.toList());
+        List<IStepRunResult> stepRunResults = this.stepRunResults.stream().filter(f->f.name().equals(stepName) && !f.result().equals(StepResult.FAILURE)).collect(Collectors.toList());
         if(stepRunResults.isEmpty())
             return Duration.ZERO;
         return Duration.ofMillis((long)stepRunResults.stream().map(f->f.duration()).mapToLong(Duration::toMillis).average().orElse(0));
     }
 
     public Integer getFlowRunCount(String flowName){
-        return flowRunResults.stream().filter(f->f.name().equals(flowName)).collect(Collectors.toList()).size();
+        return flowRunResults.stream().filter(f->f.name().equals(flowName) && !f.result().equals(FlowResult.FAILURE)).collect(Collectors.toList()).size();
     }
 
     public Integer getStepRunCount(String stepName){
-        return stepRunResults.stream().filter(f->f.name().equals(stepName)).collect(Collectors.toList()).size();
+        return stepRunResults.stream().filter(f->f.name().equals(stepName) && !f.result().equals(StepResult.FAILURE)).collect(Collectors.toList()).size();
     }
 
     public void clear() {
