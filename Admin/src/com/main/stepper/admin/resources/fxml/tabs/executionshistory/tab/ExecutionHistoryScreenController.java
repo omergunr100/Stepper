e777b5a2db926505f8bebd0e8dbfc92d90dd18fa
Omer Erlichman <omer.er10@gmail.com>
2023-07-26 00:17:22 +0300

    deleted old admin components

--- Admin/src/com/main/stepper/admin/resources/fxml/tabs/executionshistory/tab/ExecutionHistoryScreenController.java
+++ Admin/src/com/main/stepper/admin/resources/fxml/tabs/executionshistory/tab/ExecutionHistoryScreenController.java
@@ -1,11 +1,9 @@
 package com.main.stepper.admin.resources.fxml.tabs.executionshistory.tab;
 
 import com.main.stepper.admin.resources.data.PropertiesManager;
-import com.main.stepper.admin.resources.fxml.reusable.executionelements.FlowExecutionElementsController;
 import com.main.stepper.admin.resources.fxml.reusable.executioninfotree.ExecutionInfoTreeController;
 import com.main.stepper.admin.resources.fxml.reusable.executionstepsbox.ExecutionStepsBoxController;
 import com.main.stepper.admin.resources.fxml.reusable.selector.SelectorController;
-import com.main.stepper.admin.resources.fxml.reusable.stepdetails.StepDetailsController;
 import com.main.stepper.admin.resources.fxml.tabs.executionshistory.flowrundetails.FlowRunDetailsController;
 import javafx.fxml.FXML;
 
